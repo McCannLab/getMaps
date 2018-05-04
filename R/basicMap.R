7ee009c29913704ebcac0822eb359cdf323c6216
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-05-03 21:52:36 -0400

    basic map

---
+++ R/basicMap.R
@@ -0,0 +1,15 @@
+library(raster)
+
+
+## Data (see getData)
+ONT <- getData(country = "CAN", level = 1, path = "data/")[11,]
+JPN <- getData(country = "JPN", level = 0, path = "data/")
+SWE <- getData(country = "SWE", level = 0, path = "data/")
+
+
+png("fig/basic.png", unit = "in", res = 300, width = 9, height = 3.5)
+par(mfrow = c(1,3))
+plot(ONT, main = "Ontario")
+plot(JPN, main = "Japan")
+plot(SWE, main = "Sweden")
+dev.off()
