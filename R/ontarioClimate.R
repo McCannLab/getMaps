7550d65204f8c1aa0f7735cb4f919f1a79084d4e
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-08-16 16:43:45 -0400

    add few comments

--- R/ontarioClimate.R
+++ R/ontarioClimate.R
@@ -1,15 +1,20 @@
 library(raster)
 
+## Ontario boundaries
 bouONT <- getData(country='CAN', level=1, path="data/")[11,]
 
-ls_mxt <- list()
 
+## Create a list of raster
+ls_mxt <- list()
 k <- 0
 for (year in 2008:2012) {
   k <- k + 1
   ra <- raster(paste0("climateData/", year, "/maxt_07.asc"))
+  ## crop
   ls_mxt[[k]] <- rasterize(x = bouONT, y = crop(ra, bouONT@bbox), mask = T)
 }
 
+## get a raster stack
 mxt_st <- do.call(stack, ls_mxt)
+
 plot(mxt_st, col = colorRampPalette(c("#0c233e", "#4fa9d0", "#f2eeae"))(256))
--- R/retrieveClimatData.R
+++ R/retrieveClimatData.R
@@ -1,10 +1,14 @@
 dir.create("climateData/")
 
+#ftp://ftp.nrcan.gc.ca/pub/outgoing/NAM_grids
 basurl <- "ftp://ftp.nrcan.gc.ca/pub/outgoing/NAM_grids/zipfiles300/"
+# one among "bio", "cmi", "mint", "maxt", "pcp", "sg"
 info <- "maxt"
+# either "_300arcsec.zip" or "_60arcsec.zip"
+# 300 = 10 squared kilometers. 60 = 2 squred kilometers
 end <- "_300arcsec.zip"
 
-
+# year available: from 1950 to 2013
 for (year in 2008:2012) {
   zout <- paste0("climateData/", info, year, ".zip")
 
