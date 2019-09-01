838a8b6c2d49625763a183babb8a5b897f972348
Kevin Cazelles <kcazelle@uoguelph.ca>
2019-09-01 15:08:33 -0400

    add lower case letters to identify panels

--- R/map_homogenization.R
+++ R/map_homogenization.R
@@ -34,7 +34,9 @@ mygre <- 'grey50'
 mypal <- colorRampPalette(c('#f0dec3', '#361f09'))(ncolor)
 
 
-png(file = 'fig/map.png', res = 900, units = 'mm', width = 180, height = 90)
+
+
+png(file = 'fig/fig1_map_homogen.png', res = 900, units = 'mm', width = 180, height = 90)
 #
 layout(rbind(c(2, 1, 3, 4), c(2, 1, 5, 6)), width = c(.26, 1.05, .55, .24))
 par(las = 1, xaxs = 'i', yaxs = 'i', mar = rep(2.6, 4), tcl = -.4)
@@ -63,12 +65,17 @@ axis(3L, at = seq(-95, -75, 5), labels = paste0(rev(seq(75,95,5)), "°W"))
 axis(2L, at = seq(45, 55, 5), labels = paste0(seq(45,55,5), "°N"))
 axis(4L, at = seq(45, 55, 5), labels = paste0(seq(45,55,5), "°N"))
 box2(1:4, lwd = 1.1)
+# 
+mtext(expression(bold("(a)")), 3, at = -95, line = -2)
+
+
 #
 par(mar = c(4, 4, 4.5, 1.5), mgp = c(2,.8,0))
 val <- range(values(altONT), na.rm = TRUE)
 image(t(as.matrix(seq(val[1], val[2], length = ncolor))), col = mypal, axes = FALSE)
 axis(2, at = seq(0.002,.998, length = 6), labels = seq(0, 650, length = 6), lwd = 0, lwd.tick = .8, tck = -.36)
 mtext(side = 3, line = 1.2, text = 'Elevation (m)')
+
 #
 
 palc <- colorRampPalette(gpuPalette("cisl"))(ncolor)
@@ -76,11 +83,14 @@ palc <- colorRampPalette(gpuPalette("cisl"))(ncolor)
 par(mar = rep(.7, 4), xaxs = "r", yaxs = "r")
 image(mask(temp, bouCAN[9,]), axes = FALSE, ann = FALSE, col = palc)
 plot(bouCAN[9,], lwd = .8, add = TRUE)
+mtext(expression(bold("(b)")), 3, at = -95, line = -2)
+#
 par(mar = c(1, 1, 4, 4))
 valt <- range(values(temp), na.rm = TRUE)
 image(t(as.matrix(seq(valt[1], valt[2], length = ncolor))), col = palc, axes = FALSE)
 mtext(side = 3, line = 2.1, text = 'Annual', cex = .7)
 mtext(side = 3, line = .8, text = 'Temperature (Δ°C)', cex = .7)
+
 # chose based on valt
 sq <- seq(0.60, 1.9, length = 5)#' @param cex_bas magnification coefficient, controls the size of plot elements.
 
@@ -90,6 +100,8 @@ axis(4, at = scaleWithin(sq, ncolor, valt[1], valt[2])/ncolor, labels = paste0(s
 par(mar = rep(.7, 4))
 image(mask(prec, bouCAN[9,]), axes = FALSE, ann = FALSE, col = palc)
 plot(bouCAN[9,], lwd = .7, add = TRUE)
+mtext(expression(bold("(c)")), 3, at = -95, line = -2)
+#
 par(mar = c(1, 1, 4, 4))
 sq <- seq(-160, 160, length = 5)
 valp <- range(values(prec), na.rm = TRUE)
